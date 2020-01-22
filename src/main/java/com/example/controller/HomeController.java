96ff20320aaea8e7cfbd9376d9c1b8173e55acf3
jisuiyang <jisuiyang@yeah.net>
2020-01-22 16:10:49 +0800

    first page init

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/index")
+    public String index(){
+        logger.info("this is home page index");
+        return "index";
+
+    }
+
+
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jisuiyang
+  Date: 2020/1/22
+  Time: 16:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+this is home index page
+</body>
+</html>
